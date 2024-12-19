7fcb26f1bf6a3a5419399e4f58e824495a3c90cf
DivyanshTiwari20 <tmadhuvijay1982@gmail.com>
2024-12-19 12:30:15 +0530

    updated bio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,10 +10,14 @@ function Home() {
         <h1>hey there👋</h1>
         <p><hr />
         <br />
-          I'm <strong >Divyansh Tiwari</strong>, a 20 y/o Tech enthusiast from India, crafting things for the internet using the technologies I know and learn new stuff everyday. I mostly work with <strong>Python && C </strong> as my coding language. Meanwhile I am comfortable with React, bootstrap, flask etc for Web dev and some.
+          Hi I'm <strong >Divyansh Tiwari</strong>, a 20 y/o Tech enthusiast from India, crafting things for the internet using the technologies I know and learn new stuff everyday. I mostly work with <strong>Python && C </strong> as my coding language. Meanwhile I am comfortable with React, bootstrap, flask etc for Web dev also i know ML and other stuff too.
+
         </p>
         <p>
           Presently I'm doing my <strong>Btech in Computer Science</strong>, Simultaneously making some projects that can gives kick to me.
+        </p>
+        <p>
+        If you wanna hire me work your work as an freelance or some other project you can connect with me on WhatsApp <strong>8726506500.</strong>
         </p>
       </section>
       <section className="my-5">
