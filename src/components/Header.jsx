80658ad44c057ff7fb7493a0641d76ba252cdf36
DivyanshTiwari20 <tmadhuvijay1982@gmail.com>
2024-12-21 20:52:44 +0530

    hi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-
 function Header() {
   return (
     <Navbar bg="" variant="dark" expand="lg">
       <Container className="justify-content-between">
-        <Navbar.Brand href="/">Divyansh.</Navbar.Brand>
+        {/* Changed href to as and Link for Navbar.Brand */}
+        <Navbar.Brand as={Link} to="/">Divyansh.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
-            <Nav.Link href="/About">about</Nav.Link>
-            <Nav.Link href="/BlogList">blog</Nav.Link>
-            <Nav.Link href="/Contact">contact</Nav.Link>
+            {/* Changed href to as and to props for proper routing */}
+            <Nav.Link as={Link} to="/about">about</Nav.Link>
+            <Nav.Link as={Link} to="/blog">blog</Nav.Link>
+            <Nav.Link as={Link} to="/contact">contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
