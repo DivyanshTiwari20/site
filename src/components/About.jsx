a8830fe672184b853b8094e7966d22fbe7ec6d0b
DivyanshTiwari20 <tmadhuvijay1982@gmail.com>
2024-12-21 20:53:31 +0530

    hi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
   return (
     <div className='container my-5'>
-      <img src="src/assets/cat.jpg" alt="Your Name" className="img-fluid rounded-circle mb-3" style={{width: '200px', height: '200px'}} />
+      <img src="/cat.jpg" alt="Your Name" className="img-fluid rounded-circle mb-3" style={{width: '200px', height: '200px'}} />
       <h1>About Me</h1>
       <p>I'm computer science student in Lucknow. I start learning about computer since my 8th grade then thought of becoming an engineer so I give JEE entrance exam and failed. Currently, i am completing my B.Tech simultaneously making amazing open source projects. 
 
